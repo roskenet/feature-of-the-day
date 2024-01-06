2d55a13b34c03e2b8209c1fde5fa87f733b8de62
Felix Roske <felix@roskenet.de>
2024-01-06 23:44:22 +0100

    Add db insights for db

--- pg_1/003_creating_indexes.sql
+++ pg_1/003_creating_indexes.sql
@@ -8,13 +8,18 @@ EXPLAIN ANALYZE
 SELECT * from idx_test LIMIT 10;
 
 EXPLAIN ANALYZE
-SELECT * FROM idx_test WHERE active = false ORDER BY id LIMIT 10;
+SELECT * FROM idx_test WHERE active = true ORDER BY id LIMIT 10;
 
 --| First try:
 CREATE INDEX ON idx_test(id);
+DROP INDEX idx_test_id_idx;
 
 --| Generally on 'active':
 CREATE INDEX ON idx_test(id, active);
+DROP INDEX idx_test_id_active_idx;
+
+CREATE INDEX ON idx_test(id, active);
+DROP INDEX idx_test_id_active_idx;
 
 --| Selectivity of an Index:
-CREATE INDEX ON idx_test(id, active) WHERE active = false;
+CREATE INDEX ON idx_test(id, active) WHERE active = true;
