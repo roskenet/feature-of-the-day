15c4fe418f2f176f5dcfa4f04c94af15bbf1dd06
Felix Roske <felix@roskenet.de>
2024-01-07 16:54:43 +0100

    Name idexes

--- pg_1/003_creating_indexes.sql
+++ pg_1/003_creating_indexes.sql
@@ -2,7 +2,7 @@
 SET JIT=OFF;
 
 EXPLAIN ANALYSE
-SELECT * FROM idx_test WHERE id=100000;
+SELECT * FROM idx_test WHERE id=1;
 
 EXPLAIN ANALYZE
 SELECT * from idx_test LIMIT 10;
@@ -11,15 +11,16 @@ EXPLAIN ANALYZE
 SELECT * FROM idx_test WHERE active = true ORDER BY id LIMIT 10;
 
 --| First try:
-CREATE INDEX ON idx_test(id);
-DROP INDEX idx_test_id_idx;
+CREATE INDEX my_idx_id ON idx_test(id);
+DROP INDEX my_idx_id;
+
+CREATE INDEX my_idx_active ON idx_test(active);
+DROP INDEX my_idx_active;
 
 --| Generally on 'active':
-CREATE INDEX ON idx_test(id, active);
-DROP INDEX idx_test_id_active_idx;
-
-CREATE INDEX ON idx_test(id, active);
-DROP INDEX idx_test_id_active_idx;
+CREATE INDEX my_idx_id_active ON idx_test(id, active);
+DROP INDEX my_idx_id_active;
 
 --| Selectivity of an Index:
-CREATE INDEX ON idx_test(id, active) WHERE active = true;
+CREATE INDEX my_idx_id_active_true ON idx_test(id, active) WHERE active = true;
+DROP INDEX my_idx_id_active_true;
